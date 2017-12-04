8578224d2929d9746892174ac164def9ea45fd6d
Shane Osbourne <shane.osbourne8@gmail.com>
2017-12-04 15:33:08 +0000

    use cli opts

--- cli.js
+++ cli.js
@@ -4,7 +4,10 @@ var fs = require('fs');
 var h = require('./');
 
 var cli = require("meow")({
-    help: ["Usage: $ har-report harfile.json\n"]
+    help: [
+        "Usage: $ har-report harfile.json\n",
+        "Options:   --outdir  specify the output directory [string]",
+    ]
 });
 
 if (typeof cli.input[0] !== "string") {
@@ -18,11 +21,11 @@ cli.input.forEach(function (filepath) {
     var incomingPath = path.resolve(process.cwd(), filepath);
     if (isJson) {
         incoming = require(incomingPath);
-        return h({input: incoming});
+        return h({input: incoming, config: cli.flags});
     } else {
         if (filepath.match(/\.har$/)) {
             incoming = JSON.parse(fs.readFileSync(incomingPath, 'utf8'));
-            return h({input: incoming});
+            return h({input: incoming, config: cli.flags});
         }
     }
 });
